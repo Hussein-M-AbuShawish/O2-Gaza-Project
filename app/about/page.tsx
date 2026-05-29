bfca77982e15e5f6a818679d8b098c0b20a4cf7e
HusseinMAbushawish <hussen2001108@gmail.com>
2026-05-29 21:54:59 +0300

    Setup database and rating system

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { Navbar } from "../../components/navbar";
 import { Footer } from "../../components/footer";
 import {
-  Target
+  Target,
   Eye,
   Heart,
   Award,
